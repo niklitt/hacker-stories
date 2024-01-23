a508251066a91f1c1c30e115eab4e3f7b50723fd
niklitt <nicklitterio@gmail.com>
2024-01-22 17:51:45 -0700

    feature/Fundamentals-of-React: more formatting and finished Data Fetching with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import * as React from 'react';
-import './App.css'
+import * as React from "react";
+import "./App.css";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -18,80 +20,108 @@ const useStorageState = (key, initialState) => {
 
 const initialStories = [
   {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
 ];
 
+/** Correct way to gather data */
 const getAsyncStories = () =>
-  new Promise((resolve) => 
-    setTimeout(
-      () => resolve({ data: { stories: initialStories}}), 2000
-    )
-  )
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
+  );
+
+/** Throws an error when trying to gather data */
+// const getAsyncStories = () =>
+//   new Promise((resolve, reject) => setTimeout(reject, 2000)); /** */
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
-    case 'SET_STORIES':
-      return action.payload;
-    case 'REMOVE_STORIES':
-      return state.filter(
-        (story) => action.payload.objectID !== story.objectID
-      );
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
     default:
       throw new Error();
   }
 };
 
 const App = () => {
-
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
-  const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isError, setIsError] = React.useState(false);
+  const [searchTerm, setSearchTerm] = useStorageState("search", "React");
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+  // const [isLoading, setIsLoading] = React.useState(false);
+  // const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    setIsLoading(true);
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories().then(result => {
-      dispatchStories({
-        type: 'SET_STORIES',
-        payload: result.data.stories,
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
+      .then((result) => {
+        dispatchStories({
+          type: "STORIES_FETCH_SUCCESS",
+          payload: result.hits,
+        });
       })
-      setIsLoading(false);
-    }).catch(() => setIsError(true));
+      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, []);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
-      type: 'REMOVE_STORIES',
+      type: "REMOVE_STORY",
       payload: item,
-    })
-  }
+    });
+  };
 
-  React.useEffect( () => {
-    localStorage.setItem('search', searchTerm);
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
   }, [searchTerm]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-  }
+  };
 
-  const searchedStories = stories.filter((story) => 
+  const searchedStories = stories.data.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+  );
 
   // In error handling block, if false it skips, if true it returns what's after the &&. better than ternary operator
   return (
@@ -99,20 +129,20 @@ const App = () => {
       <h1>My Hack3r Stories.</h1>
       <InputWithLabel
         id="search"
-        value = {searchTerm}
+        value={searchTerm}
         isFocused
-        onInputChange = {handleSearch} 
+        onInputChange={handleSearch}
       >
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
 
-      {isError && <p>Something went wrong with gathering data....??</p>} 
+      {stories.isError && <p>Something went wrong with gathering data....??</p>}
 
-      {isLoading ? (
+      {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
@@ -120,38 +150,45 @@ const App = () => {
 
 //No idea wtf &nbsp is used for exactly
 //autoFocus is the same as autoFocus={true}
-const InputWithLabel = ({id, value, type = 'text', onInputChange, isFocused, children}) => {
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) => {
   const inputRef = React.useRef();
 
   React.useEffect(() => {
-    if(isFocused && inputRef.current) {
+    if (isFocused && inputRef.current) {
       inputRef.current.focus();
     }
   }, [isFocused]);
   return (
     <>
       <label htmlFor={id}>{children}</label>
-      &nbsp; /* wtf is this?????? *\
-      <input 
+      &nbsp; /** wtf is this?? */
+      <input
         id={id}
         type={type}
         value={value}
-        autoFocus = {isFocused}
-        onChange={onInputChange}/>
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
     </>
   );
 };
 
-
-const List = ({list, onRemoveItem}) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
     ))}
   </ul>
 );
 
-const Item = ({item, onRemoveItem}) => {
+const Item = ({ item, onRemoveItem }) => {
   return (
     <li>
       <span>
@@ -166,7 +203,7 @@ const Item = ({item, onRemoveItem}) => {
         </button>
       </span>
     </li>
-  )
+  );
 };
 
-export default App
+export default App;
