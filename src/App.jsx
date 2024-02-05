947cdcf0086d08e7bf6220ac09e57779ad68da74
niklitt <nicklitterio@gmail.com>
2024-02-05 15:01:42 -0700

    feature/Fundamentals-of-React: completed Forms in React

--- src/App.jsx
+++ src/App.jsx
@@ -81,6 +81,26 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+        <InputWithLabel
+          id="search"
+          value={searchTerm}
+          isFocused
+          onInputChange={onSearchInput}
+        >
+          <strong>Search:</strong>
+        </InputWithLabel>
+        <button type="submit" disabled={!searchTerm}>
+          Submit
+        </button>
+      </form>
+);
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
@@ -157,8 +177,10 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
   };
 
   // const handleSearch = (event) => { // Used submit pre-button
@@ -169,17 +191,12 @@ const App = () => {
   return (
     <div>
       <h1>My Hack3r Stories.</h1>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
